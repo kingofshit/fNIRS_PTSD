6bb77111e32dc1416047b05bb30c41f7f4158010
kingofshit <1176469320@qq.com>
2024-01-26 22:08:12 +0800

    第一次提交

---
+++ CreateFolder.py
@@ -0,0 +1,11 @@
+import os
+from shutil import copyfile
+
+rootdir = "D:\\fNIRS_Data\\zishu\\ALFF&fALFF\\"
+periodlist = ["1RESTING\\","2COUNT\\","3SPEAK\\","4COUNT\\","5LISTEN\\","6COUNT\\"]
+grouplist = ["HC\\", "PTSD\\"]
+
+for period in periodlist:
+    for group in grouplist:
+        print(rootdir+period+group)
+        os.makedirs(rootdir+period+group)
